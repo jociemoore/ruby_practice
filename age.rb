ff6b586dcaa5b476ca9a179086037f921781edb3
Jocelyn Moore <hello@jociemoore.com>
2016-10-04 22:48:42 -0400

    Add Chapter 2 Exercises

---
+++ age.rb
@@ -0,0 +1,14 @@
+puts "How old are you?"
+
+age = gets.chomp
+
+tenYears = age.to_i + 10
+twentyYears = age.to_i + 20
+thirtyYears = age.to_i + 30
+fortyYears = age.to_i + 40
+
+
+puts "In 10 years you will be:\n" + tenYears.to_s
+puts "In 20 years you will be:\n" + twentyYears.to_s
+puts "In 30 years you will be:\n" + thirtyYears.to_s
+puts "In 40 years you will be:\n" + fortyYears.to_s
