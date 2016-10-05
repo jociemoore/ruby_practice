4714ada740ea6678e476a16f042f2f79fc9431f3
Jocelyn Moore <hello@jociemoore.com>
2016-10-04 23:42:46 -0400

    organized files

--- exercies6.rb
+++
@@ -1,6 +0,0 @@
-# ArgumentError: wrong number of arguments (1 for 2)
-#   from (irb):1:in `calculate_product'
-#   from (irb):4
-#   from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
-
-# This error message tells me that I have passed the method calculate_product the wrong number of arguments. Its expecting 2 arguements and only received 1.
