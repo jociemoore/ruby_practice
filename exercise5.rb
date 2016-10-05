c6e0749d60599f5955dcb7e8db640b6ab5085dec
Jocelyn Moore <hello@jociemoore.com>
2016-10-04 22:40:55 -0400

    Add Chapter 1 Exercises

---
+++ exercise5.rb
@@ -0,0 +1,9 @@
+fiveFact = 1*2*3*4*5
+sixFact = fiveFact * 6
+sevenFact = sixFact * 7 
+eightFact = sevenFact * 8
+
+puts "5! = " + fiveFact.to_s
+puts "6! = " + sixFact.to_s
+puts "7! = " +sevenFact.to_s
+puts "8! = " + eightFact.to_s
---
+++ exercise7.rb
@@ -0,0 +1,3 @@
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}' from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+# The above error message tells me that on line 2 of my program, I am most likely missing a closing bracket. The other alternative is that I put a closing parentheses when I should have put a closing curly bracket.
