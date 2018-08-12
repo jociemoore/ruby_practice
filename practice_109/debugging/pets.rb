8b69b30bac61133cc0737882b805affb3b86eb2e
Jocie Moore <hello@jociemoore.com>
2018-08-12 15:15:40 -0700

    Add pets solution

---
+++ practice_109/debugging/pets.rb
@@ -0,0 +1,20 @@
+# -----------
+# Buggy Code
+# -----------
+
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog] = 'bowser'
+
+p pets #=> {:cat=>"fluffy", :dog=>"bowser", :fish=>"oscar"}
+
+
+# -----------------
+# Refactored Code
+# -----------------
+
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog] << 'bowser'
+
+p pets #=> {:cat=>"fluffy", :dog=>['sparky', 'fido', 'bowser'], :fish=>"oscar"}
